078d46ea5e1bb2b15eee770a1d2ccc8fcfabd79b
Luisgsilva950 <luisgsilva950@gmail.com>
2020-09-22 02:56:10 -0300

    run tests ok

--- src/main.cc
+++ src/main.cc
@@ -18,13 +18,11 @@ int main() {
     List<int> broken = *new List<int>();
     List<int> war = *new List<int>();
 
-//    cout << "Insert the number of ships:" << endl;
-    scanf("%d",&N);
+   cin >> N;
 
     for (int i = 0; i < N; i++) {
         int shipId = 0;
-//        cout << "Insert the ship ID:" << endl;
-        scanf("%d",&shipId);
+        cin >> shipId;
         ships.insertOnFront(shipId);
     }
 
@@ -32,25 +30,23 @@ int main() {
     while (scanf("%d", &action) != EOF) {
         switch (action) {
             case SEND_SHIP_TO_WAR:
-                cout << "Ship " << ships.getFirst() << " on war." << endl;
+                cout << "nave " << ships.getFirst() << " em combate" << endl;
                 war.insertOnFront(ships.getFirst());
                 ships.popFirst();
                 break;
             case REPAIRED_SHIP:
-                cout << "Ship " << broken.getFirst() << " repaired." << endl;
+                cout << "nave " << broken.getFirst() << " consertada" << endl;
                 ships.insertOnFront(broken.getFirst());
                 broken.popFirst();
                 break;
             case PRINT_WAITING_SHIPS:
-                cout << "WAITING SHIPS:" << endl;
                 ships.showList();
                 break;
             case PRINT_BROKEN_SHIPS:
-                cout << "BROKEN SHIPS:" << endl;
                 broken.showList();
                 break;
             default:
-                cout << "Ship " << action << " broken." << endl;
+                cout << "nave " << action << " avariada" << endl;
                 broken.insertOnLast(action);
                 war.removeByValue(action);
                 break;
